8ee65847aa87dc21f52d01d20d97a89e52c18a0c
FernandoGmz2001 <fernandogmz2001@gmail.com>
2022-10-17 15:32:00 -0500

    JQuery

--- Parcial 2/DelegacionEvento/main.js
+++ Parcial 2/DelegacionEvento/main.js
@@ -3,3 +3,18 @@ const contenedor = document.querySelector("#cont");
 contenedor.addEventListener("click", (e) => {
   e.target.classList.toggle("darkMode");
 });
+
+$(document).ready(function () {
+  $("#cont").click(function (e) {
+    // alert("se escucho el click en la libreria Jquery");
+    console.log(e.target);
+    // console.log("Me hiciste click");
+    $(e.target).html("Evento delegacion");
+  });
+});
+
+// window.onload = () => {
+//   document
+//     .querySelector("#cont")
+//     .addEventListener("click", () => console.log(e.target));
+// };
