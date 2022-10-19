227d43c12c409c2a11a487fdf29c1d556080f4f6
FernandoGmz2001 <fernandogmz2001@gmail.com>
2022-10-18 19:00:47 -0500

    Grid Bootstrap

--- Parcial 2/DelegacionEvento/main.js
+++ Parcial 2/DelegacionEvento/main.js
@@ -6,15 +6,7 @@ contenedor.addEventListener("click", (e) => {
 
 $(document).ready(function () {
   $("#cont").click(function (e) {
-    // alert("se escucho el click en la libreria Jquery");
     console.log(e.target);
-    // console.log("Me hiciste click");
     $(e.target).html("Evento delegacion");
   });
 });
-
-// window.onload = () => {
-//   document
-//     .querySelector("#cont")
-//     .addEventListener("click", () => console.log(e.target));
-// };
