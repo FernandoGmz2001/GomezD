7a4ebc6b6f4a09259aef34cacf2f5ba99a572018
FernandoGmz2001 <fernandogmz2001@gmail.com>
2022-11-19 14:18:09 -0600

    Comentarios borrados Formulario Bootstrap

--- Parcial 3/FormularioBootstrap/configuracion.php
+++ Parcial 3/FormularioBootstrap/configuracion.php
@@ -1,15 +1,4 @@
 <?php
-// $Nombre= "Fernando";
-// $ApellidoMaterno = "Gomez";
-// $ApellidoPaterno = "Dominguez";
-// $Correo = "fernandogmz2001@gmail.com";
-// $Ciudad = "Nuevo Laredo";
-// $Estado = "Tamaulipas";
-// $Direccion = "Los fresnos";
-// $Telefono = "867 752 9509";
-// $Password = "12345";
-// $ConfirmPass = "12345";
-
 $registro["Nombre"]= "Fernando";
 $registro["ApellidoMaterno"] = "Gomez";
 $registro["ApellidoPaterno"] = "Dominguez";
@@ -23,8 +12,6 @@ $registro["ConfirmPass"] = "12345";
 
 
 echo json_encode($registro);
-
-// echo json_encode(array('Nombre' => $Nombre, 'ApellidoPaterno' => $ApellidoPaterno, 'ApellidoMaterno'=> $ApellidoMaterno, 'Correo' =>$Ciudad, 'Estado'=>$Estado, 'Direccion'=>$Direccion, 'Telefono'=> $Telefono, 'Password' => $pPassword, 'ConfirmPass'=>$ConfirmPass));
 ?>
 
 
