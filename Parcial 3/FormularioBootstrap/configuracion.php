c8757dd9951ab739cc4205599cb20ebfa4cc6e2e
FernandoGmz2001 <fernandogmz2001@gmail.com>
2022-11-18 22:57:16 -0600

    Se agrego el php con datos hardcodeados

---
+++ Parcial 3/FormularioBootstrap/configuracion.php
@@ -0,0 +1,14 @@
+<?php
+$registro["Nombre"]= "Fernando";
+$registro["ApellidoMaterno"] = "Gomez";
+$registro["ApellidoPaterno"] = "Dominguez";
+$registro["Correo"] = "fernandogmz2001@gmail.com";
+$registro["Ciudad"] = "Nuevo Laredo";
+$registro["Estado"] = "Tamaulipas";
+$registro["Direccion"] = "Los fresnos";
+$registro["Telefono"] = "867 752 9509";
+$registro["Password"] = "12345";
+$registro["ConfirmPass"] = "12345";
+
+echo json_encode($registro);
+?>
---
+++ Parcial 3/FormularioBootstrap/index.js
@@ -0,0 +1,20 @@
+const btnConfirmar = document.querySelector(".btnConfirmar");
+const formulario = document.querySelector(".formulario");
+
+btnConfirmar.addEventListener("click", async (e) => {
+  console.log("Hola");
+  let respuesta = await fetch("configuracion.php");
+  let dato = await respuesta.json();
+  document.getElementByIdi("nombre").value = reg.Nombre;
+  document.getElementById("apellidoPaterno").value = reg.ApellidoMaterno;
+  document.getElementById("apellidoMaterno").value = reg.ApellidoPaterno;
+  document.getElementById("emailInput").value = reg.Correo;
+  document.getElementById("ciudadInput").value = reg.Ciudad;
+  document.getElementById("estadoInput").value = reg.Estado;
+  document.getElementById("direccionInput").value = reg.Direccion;
+  document.getElementById("telefonoInput").value = reg.Telefono;
+  document.getElementById("inputPass").value = reg.Password;
+  document.getElementById("confirmPassInput").value = reg.ConfirmPass;
+
+  window.alert("Se pudo realizar el registro exitosamente");
+});
